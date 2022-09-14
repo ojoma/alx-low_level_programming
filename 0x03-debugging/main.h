153d3f9a7ad856c069672994f1490ecf0ef7d7b9
Hassan M <ojomamd@gmail.com>
2022-09-14 11:53:38 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
+/*
+ * 
+ * 
+ * Desc: Header file containing prototypes to out positive or negative number
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
 
 void positive_or_negative(int i);
 
