8d5c41a097af5c59669428bb02d852a6beccdc1f
Hassan M <ojomamd@gmail.com>
2022-09-19 19:10:38 -0700

    atoi

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 char _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
