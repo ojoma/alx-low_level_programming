23c09558066897008526bfdf2cd21cc5b508908c
Hassan M <ojomamd@gmail.com>
2022-09-19 18:12:05 -0700

    half

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 char _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
