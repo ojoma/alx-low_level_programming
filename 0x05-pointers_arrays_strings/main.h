c23156a4b757f4b4b77df7a018c7f78da15b829e
Hassan M <ojomamd@gmail.com>
2022-09-19 18:16:26 -0700

    array_print

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
 char _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
