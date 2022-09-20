10cf457f7fc781814602c924703cd634db7e51e7
Hassan M <ojomamd@gmail.com>
2022-09-19 18:07:31 -0700

    counts_chr

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
 char _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
