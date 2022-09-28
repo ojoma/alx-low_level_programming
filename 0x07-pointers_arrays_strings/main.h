6acfa9d8be691f630d39abcc24fc6e0e0d6cebaa
Hassan M <ojomamd@gmail.com>
2022-09-27 18:12:56 -0700

    0-memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,15 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-	/**
-	 * File: main.h
-	 * Auth: Hassan Awodi
-	 * Desc: Header file containting prototypes for all functions used in 
-	 * 	0x07-pointers_arrays_strings projects.
-	 *
-	 */
-#include <stddef.h>
-
+int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+
+#endif
+
 
