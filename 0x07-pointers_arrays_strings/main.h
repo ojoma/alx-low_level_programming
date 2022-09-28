d58845edc0b7f580b20596b1ab4ce231a5ce1fda
Hassan M <ojomamd@gmail.com>
2022-09-27 18:37:42 -0700

    2-strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -38,5 +38,6 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
