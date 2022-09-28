1b2dcba5161a7f17b6e36ae0afa970699e2807e3
Hassan M <ojomamd@gmail.com>
2022-09-27 18:23:52 -0700

    1-memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -37,6 +37,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 char *_memset(char *s, char b, unsigned int n);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
