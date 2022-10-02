0240b069e6a6aa2094dc6c203d6fbcb5ba38f5ec
Hassan M <ojomamd@gmail.com>
2022-10-02 15:49:15 -0700

    static library

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _isdigit(int c);
 int _isupper(int c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -52,4 +53,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 #endif /* MAIN_H */
