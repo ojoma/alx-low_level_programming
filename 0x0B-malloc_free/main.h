844ea87b43df19091a2652aef9df9820d8788be9
Hassan M <ojomamd@gmail.com>
2022-10-03 04:33:06 -0700

    0-create_array

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
